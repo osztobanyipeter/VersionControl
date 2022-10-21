92a877ef39fcbf0c4390176279a6ff98032150af
Péter Osztobányi <peter.osztobanyi@stud.uni-corvinus.hu>
2022-10-21 17:29:10 +0200

    negyedik

--- UserMaintenance/5.50/Form1.cs
+++ UserMaintenance/5.50/Form1.cs
@@ -1,4 +1,5 @@
-﻿using _5._50.MnbServiceReference;
+﻿using _5._50.Entities;
+using _5._50.MnbServiceReference;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,6 +14,7 @@ namespace _5._50
 {
     public partial class Form1 : Form
     {
+        BindingList<RateData> Rates = new BindingList<RateData>();
         public Form1()
         {
             InitializeComponent();
@@ -30,7 +32,11 @@ namespace _5._50
             };
             var response = mnbService.GetExchangeRates(request);
             var result = response.GetExchangeRatesResult;
+
+            dataGridView1.DataSource = Rates.ToList();
+
         }
+        
 
     }
 }
